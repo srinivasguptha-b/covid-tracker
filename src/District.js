3c2980547e689ac81424e0869d176523cc6e7d8a
srinivas <srinivasguptha.b@gmail.com>
2021-09-14 19:24:58 +0530

    Aishu code merged

--- src/District.js
+++ src/District.js
@@ -4,39 +4,38 @@ import { Container, Row, Col, Card, Table } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 
 function District(props) {
-  const [values,setValues]=useState([]);
-  const [stateValue,setstateValue]=useState(false);
-  const [selectedValue,setSelectedValue]=useState([]);  
-  let object = {"0":5,"1":7,"2":4,"3":6,"4":7,"5":8,"6":12,"7":11,"8":2}  
-  let stateSelected=props.stateSelected;
-  useEffect(()=>{
-    setstateValue(false);   
+  const [values, setValues] = useState([]);
+  const [stateValue, setstateValue] = useState(false);
+  const [selectedValue, setSelectedValue] = useState([]);
+  let object = { "0": 5, "1": 7, "2": 4, "3": 6, "4": 7, "5": 8, "6": 12, "7": 11, "8": 2 }
+  let stateSelected = props.stateSelected;
+  useEffect(() => {
+    setstateValue(false);
 
-    fetch('https://data.covid19india.org/v4/min/data.min.json').then(x=>x.json()).then(x=>{    
-      
-        Object.entries(x).map(([key, val]) => 
-        {  
-         // console.log(val.meta.last_updated)      ;
-          //console.log(val)
-         if(key==stateSelected){ 
-           let array=[];
-          Object.entries(val.districts).map(value=>{              
-            array.push(value);        
-          })   
+    fetch('https://data.covid19india.org/v4/min/data.min.json').then(x => x.json()).then(x => {
+
+      Object.entries(x).map(([key, val]) => {
+        // console.log(val.meta.last_updated)      ;
+        //console.log(val)
+        if (key == stateSelected) {
+          let array = [];
+          Object.entries(val.districts).map(value => {
+            array.push(value);
+          })
           setSelectedValue(array);
           setstateValue(true);
-         }         
-        })       
-      setValues(x);          
+        }
+      })
+      setValues(x);
     });
-  },[stateSelected])  
-  const TableDisplay=()=>{    
-  //  console.log(selectedValue)  
-    let display=<div></div> 
-    if(stateValue){
-      display= (<div>  
-        <br/>       
-        <Table>
+  }, [stateSelected])
+  const TableDisplay = () => {
+    //  console.log(selectedValue)  
+    let display = <div></div>
+    if (stateValue) {
+      display = (<div>
+        <br />
+        <Table className="table table-striped">
           <thead>
             <tr>
               <th>
@@ -59,45 +58,45 @@ function District(props) {
               </th>
             </tr>
           </thead>
-     {/* {JSON.stringify(selectedValue[0])} */}
-     <tbody>
-         {selectedValue.map(value=>{
-           return (
-            <tr key={value[0]}>
-              <td>              
-               {value[0]}
+          {/* {JSON.stringify(selectedValue[0])} */}
+          <tbody>
+            {selectedValue.map(value => {
+              return (
+                <tr key={value[0]}>
+                  <td>
+                    {value[0]}
+                  </td>
+                  <td>
+                    {value[1].total.confirmed}
+                  </td>
+                  <td>
+                    {value[1].total.confirmed ? !(value[1].total.deceased) ? (Number(value[1].total.confirmed) - Number(value[1].total.recovered)) : (Number(value[1].total.confirmed) - (Number(value[1].total.recovered) + Number(value[1].total.deceased))) : 0}
+                  </td>
+                  <td>
+                    {value[1].total.recovered ? Number(value[1].total.recovered) : 0}
+                  </td>
+                  <td>
+                    {!(value[1].total.deceased) ? 0 : value[1].total.deceased}
+                  </td>
+                  <td>
+                    -
               </td>
-              <td>
-                {value[1].total.confirmed}
-              </td>
-              <td>                              
-                {!(value[1].total.deceased)?(Number(value[1].total.confirmed)-Number(value[1].total.recovered)):(Number(value[1].total.confirmed)-(Number(value[1].total.recovered)+Number(value[1].total.deceased)))}
-              </td>
-              <td>
-                {Number(value[1].total.recovered)}  
-              </td>
-              <td>
-              {!(value[1].total.deceased)?0:value[1].total.deceased}
-              </td>
-              <td>
-               -
-              </td>
-            </tr>
-            )
-          })} 
-        </tbody> 
+                </tr>
+              )
+            })}
+          </tbody>
         </Table>
       </div>)
     }
-    else{
-      display=<div></div>
+    else {
+      display = <div></div>
     }
-   return display;
+    return display;
   }
-  return (    
-   <div> 
-    <TableDisplay/>
-   </div>
+  return (
+    <div>
+      <TableDisplay />
+    </div>
   );
 }
 
